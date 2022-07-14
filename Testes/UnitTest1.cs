8cd489f310902cab0f7c6d809dc4b8d860dec5d0
Roberto Paes <robertocpaes@gmail.com>
2022-07-14 20:49:45 -0300

    Resolvendo problema de divergência de culture entre plataformas

--- Testes/UnitTest1.cs
+++ Testes/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace Testes
     {
         //executar a cada minuto
 
+        [UseCulture("pt-BR")]
         [Theory]
         [InlineData("* * * * *")]
         public void ExpressaoSimples(string cronExpression)
@@ -20,6 +21,7 @@ namespace Testes
         }
 
         //executar a cada cinco minutos
+        [UseCulture("pt-BR")]
         [Theory]
         [InlineData("5 * * * *")]
         public void ExpressaoMinutosEspecificos(string cronExpression)
@@ -32,6 +34,7 @@ namespace Testes
         }
 
         //executar caso seja um range entre minutos 20:01 e 20:05
+        [UseCulture("pt-BR")]
         [Theory]
         [InlineData("1-5 * * * *", "11/07/2022 20:00:00", "11/07/2022 20:01:00")]
         public void ExpressaoMinutosRange(string cronExpression, string dataInicio, string dataFinal)
@@ -44,6 +47,7 @@ namespace Testes
             Assert.True(cronJob.IsDispatchTime(dataDisparo));
         }
         //executar toda segunda feira, às 14:30
+        [UseCulture("pt-BR")]
         [Theory]
         [InlineData("30 14 * * 1", "11/07/2022 12:00:00", "18/07/2022 14:30:00")]
         [InlineData("30 14 * * 1", "13/07/2022 12:00:00", "18/07/2022 14:30:00")]
@@ -59,6 +63,7 @@ namespace Testes
             Assert.True(cronJob.IsDispatchTime(dataDisparo));
         }
 
+        [UseCulture("pt-BR")]
         [Theory]
         [InlineData("0 22 * * 1-5", "13/07/2022 18:22:00", "13/07/2022 22:00:00")]
         public void ExpressaoComplexaDois(string cronExpression, string dataInicio, string dataFinal)
@@ -71,6 +76,7 @@ namespace Testes
             Assert.True(cronJob.IsDispatchTime(dataDisparo));
         }
 
+        [UseCulture("pt-BR")]
         [Theory]
         [InlineData("* * 14-17 * *", "14/07/2022 19:56:00", "14/07/2022 19:57:00")]
         public void ExpressaoComplexaTres(string cronExpression, string dataInicio, string dataFinal)
