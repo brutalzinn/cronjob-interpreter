8fa39527af771b2b60f96834a2580cb618c1a382
Roberto Paes <robertocpaes@gmail.com>
2022-07-14 19:58:17 -0300

    deploy

--- Testes/UnitTest1.cs
+++ Testes/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Testes
     {
         //executar a cada minuto
 
-        [Theory(Skip = "Pulando")]
+        [Theory]
         [InlineData("* * * * *")]
         public void ExpressaoSimples(string cronExpression)
         {
@@ -20,7 +20,7 @@ namespace Testes
         }
 
         //executar a cada cinco minutos
-        [Theory(Skip = "Pulando")]
+        [Theory]
         [InlineData("5 * * * *")]
         public void ExpressaoMinutosEspecificos(string cronExpression)
         {
@@ -32,7 +32,7 @@ namespace Testes
         }
 
         //executar caso seja um range entre minutos 20:01 e 20:05
-        [Theory(Skip = "Pulando")]
+        [Theory]
         [InlineData("1-5 * * * *", "11/07/2022 20:00", "11/07/2022 20:01")]
         public void ExpressaoMinutosRange(string cronExpression, string dataInicio, string dataFinal)
         {
@@ -44,7 +44,7 @@ namespace Testes
             Assert.True(cronJob.IsDispatchTime(dataDisparo));
         }
         //executar toda segunda feira, às 14:30
-        [Theory(Skip= "Pulando")]
+        [Theory]
         [InlineData("30 14 * * 1", "11/07/2022", "18/07/2022 14:30")]
         [InlineData("30 14 * * 1", "13/07/2022", "18/07/2022 14:30")]
         [InlineData("30 14 * * 1", "18/07/2022", "25/07/2022 14:30")]
@@ -59,9 +59,21 @@ namespace Testes
             Assert.True(cronJob.IsDispatchTime(dataDisparo));
         }
 
-        [Theory(Skip = "Pulando")]
+        [Theory]
         [InlineData("0 22 * * 1-5", "13/07/2022 18:22", "13/07/2022 22:00")]
         public void ExpressaoComplexaDois(string cronExpression, string dataInicio, string dataFinal)
+        {
+            var dataTime = DateTime.Parse(dataInicio);
+            var dataDisparo = DateTime.Parse(dataFinal);
+
+            var cronJob = new CronJob(cronExpression, dataTime);
+
+            Assert.True(cronJob.IsDispatchTime(dataDisparo));
+        }
+
+        [Theory]
+        [InlineData("* * 14-17 * *", "14/07/2022 19:56", "14/07/2022 19:57")]
+        public void ExpressaoComplexaTres(string cronExpression, string dataInicio, string dataFinal)
         {
             var dataTime = DateTime.Parse(dataInicio);
             var dataDisparo = DateTime.Parse(dataFinal);
